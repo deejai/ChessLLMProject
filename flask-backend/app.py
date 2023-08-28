e890c245c7e07b5223b04f426b39d1ab7f12d8f2
deejai <deejai@live.com>
2023-08-28 00:28:20 -0600

    trying to fix api

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, jsonify, request
+from flask_cors import CORS
 import os
 import json
-
+import openai
+from dotenv import load_dotenv
 from database_connectors.sqlite_db import SQLiteDatabaseConnection
+
+load_dotenv()
 
 bp = Blueprint('chess-llm-coach-api', __name__, template_folder='templates')
 app = Flask(__name__)
+CORS(app, resources={r"/chess-llm-coach-api/*": {"origins": "robotchesscoach.com"}})
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 sqlite_db = SQLiteDatabaseConnection("db.sqlite3")
 
@@ -22,10 +29,32 @@ def test():
         status=200,
         mimetype='application/json'
     )
+    return response
 
-    print(results)
+@bp.route('/ask', methods=['POST'])
+def ask_gpt():
+    user_input = request.json.get('prompt')
+    model_engine = "gpt-4.0"
+    conversation = [
+        {"role": "user", "content": user_input}
+    ]
+    openai_response = openai.ChatCompletion.create(
+        model=model_engine,
+        messages=conversation
+    )
+    gpt_response = openai_response['choices'][0]['message']['content']
+
+    response = app.response_class(
+        response=json.dumps({"data": {"response": gpt_response}}),
+        status=200,
+        mimetype='application/json'
+    )
 
     return response
+
+@bp.route('/ask-form')
+def ask_form():
+    return render_template('ask_gpt.html')
 
 with app.app_context():
     app.register_blueprint(bp, url_prefix='/chess-llm-coach-api')
