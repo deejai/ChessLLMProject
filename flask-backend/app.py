ae94001dc4db7966d59129e34eecad185b16fffa
deejai <deejai@live.com>
2023-08-29 17:56:43 -0600

    fixed CORS?

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -13,8 +13,9 @@ from chess_coach.stockfish.utilities import is_valid_fen
 load_dotenv()
 
 bp = Blueprint('chess-llm-coach-api', __name__, template_folder='templates')
+CORS(bp, origins=["https://robotchesscoach.com"])
+
 app = Flask(__name__)
-CORS(app, resources={r"/chess-llm-coach-api/*": {"origins": "robotchesscoach.com"}})
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
