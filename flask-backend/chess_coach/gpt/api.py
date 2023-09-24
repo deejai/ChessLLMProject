0b0db9e6b21a8d7ceb6af738f4599565d6ce527f
deejai <deejai@live.com>
2023-09-24 17:09:43 -0600

    back to gpt 4

--- flask-backend/chess_coach/gpt/api.py
+++ flask-backend/chess_coach/gpt/api.py
@@ -1,8 +1,8 @@
 import openai
 
 def ask_gpt(prompt):
-    model_engine = "gpt-3.5-turbo"
-    # model_engine = "gpt-4"
+    # model_engine = "gpt-3.5-turbo"
+    model_engine = "gpt-4"
     conversation = [
         {"role": "user", "content": prompt}
     ]
